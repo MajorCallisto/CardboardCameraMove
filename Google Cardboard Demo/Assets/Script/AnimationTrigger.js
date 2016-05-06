71bb27edb4dc7d10e5d1ab6c6745b43194e225c5
blacksanta69 <clewis@inthecolorfield.com>
2016-05-06 19:21:23 -0400

    Initial push

---
+++ Google Cardboard Demo/Assets/Script/AnimationTrigger.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+//Christopher Lewis initial build May 06, 2016
+
+//This script is controlled with Event Triggers that can be found on the animation clip timeline OR the sphere object
+
+public var curtainAnimator:Animator;//The animator to play
+public var canvasCurtain:GameObject;//the canvas that the curtain is located in. Active is toggled so this doesn't interfere with the reticle
+public var cardboardMain:GameObject;//the gameobject to move
+private var moveToStart:boolean = false;//a variable used to toggle whether the camera goes to zero or elsewhere
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+public function curtainIn(){
+	canvasCurtain.SetActive(true);
+	curtainAnimator.Play("FadeIn");
+}
+public function curtainOut(){
+	curtainAnimator.Play("FadeOut");
+}
+public function moveCamera(){
+	if (moveToStart == true){
+		cardboardMain.transform.position.x = 0;
+	}else{
+		cardboardMain.transform.position.x = 3.39;
+	}
+	moveToStart = !moveToStart;
+	curtainOut();
+}
+public function disableCanvas(){
+	canvasCurtain.SetActive(false);
+}
---
+++ Google Cardboard Demo/Assets/Script/LookatGeometry.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+
+private var carrying:boolean = false;
+function Start () {
+	var textObj:UnityEngine.UI.Text = GameObject.Find("Hide Button Text").GetComponent.<UnityEngine.UI.Text>();
+	textObj.text = "Hi there";
+	Debug.Log (textObj);
+}
+
+function Update () {
+
+}
+public function SetGazedAt(gazedAt:boolean) {
+	GetComponent.<Renderer>().material.color = gazedAt ? Color.green : Color.white;
+
+	if (gazedAt == false){
+		carrying = false;
+		dropIt();
+	}
+}
+public function pickup(){
+	if (carrying == false){
+		GetComponent.<Rigidbody>().useGravity = false;
+		transform.parent = GameObject.Find("Cardboard Reticle").transform;
+		carrying = true;
+	}else{
+		dropIt();
+	}
+}
+public function dropIt(){
+	GetComponent.<Rigidbody>().useGravity = true;
+	transform.parent = GameObject.Find("Group Interactive").transform;
+	carrying = false;
+}
